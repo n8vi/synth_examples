5935763db849cacca37571de4f435903f88914fc
Brad Tarratt <brad.tarratt@p3-group.com>
2018-09-06 09:58:30 -0400

    added filter frequency response plotter

---
+++ plot.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+import matplotlib.pyplot as plt
+import math
+
+import sys
+import random
+import struct
+
+class reslp:
+    lastOutput = 0
+    momentum = 0
+
+    def reslp(this,samp):
+        distanceToGo = samp - this.lastOutput;
+        this.momentum += distanceToGo * 0.125 # * 0.0001
+        this.lastOutput += this.momentum + distanceToGo * 0.125
+        return this.lastOutput * 0.7
+
+r = reslp()
+xvals = []
+yvals = []
+
+t = range(0, 44100)
+
+for f in range (0,8820, 50):
+    s = [math.cos(2 * math.pi * (sam/44100.0) * f) for sam in t]
+    fr = [r.reslp(sam) for sam in s]
+    sq = [sam*sam for sam in fr]
+    rms = math.sqrt(sum(sq)/len(sq))
+    xvals.append(f)
+    yvals.append(rms)
+    print("Frequency %f | Amplitude %f"%(f, rms))
+
+fig, ax = plt.subplots()
+ax.plot(xvals,yvals)
+ax.set(xlabel="frequency", ylabel="amplitude", title="Frequency Response")
+ax.grid()
+plt.show()
